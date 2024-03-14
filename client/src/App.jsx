1416a6cde5373ea302078c5eb47c9352646359fd
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-14 03:35:45 +0100

    Creating components for app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+// - React -
 import React from "react";
+import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
+// - Components -
+import HomePage from "./components/HomePage/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 const App = () => {
-  return <div>Jobify App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ client/src/components/DeleteJobs/DeleteJobs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteJobs = () => {
+  return <div>DeleteJobs</div>;
+};
+
+export default DeleteJobs;
