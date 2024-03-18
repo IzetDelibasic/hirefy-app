ca54ca8cce63060923ec5e0a073956af44ae36c6
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-18 02:29:51 +0100

    Creating logic for login routing

--- client/src/components/FomRow/FormRow.jsx
+++ client/src/components/FomRow/FormRow.jsx
@@ -1,21 +1,27 @@
-export const FormRow = ({ type, name, labelText, defaultValue }) => {
-  return (
-    <div className="mb-4 w-[15rem]">
-      <label
-        htmlFor={name}
-        className="font-normal uppercase font-montserrat text-[14px]"
-      >
-        {labelText || name}
-      </label>
-      <input
-        type={type}
-        name={name}
-        defaultValue={defaultValue || ""}
-        className="w-full bg-gray-800 text-white border-none rounded-lg px-4 py-3"
-        required
-      />
-    </div>
-  );
-};
+import React from "react";
+
+const FormRow = React.forwardRef(
+  ({ type, name, labelText, defaultValue, onChange }, ref) => {
+    return (
+      <div className="mb-4 w-[15rem]">
+        <label
+          htmlFor={name}
+          className="font-normal uppercase font-montserrat text-[14px]"
+        >
+          {labelText || name}
+        </label>
+        <input
+          type={type}
+          name={name}
+          defaultValue={defaultValue || ""}
+          ref={ref}
+          className="w-full bg-gray-800 text-white border-none rounded-lg px-4 py-3"
+          onChange={onChange}
+          required
+        />
+      </div>
+    );
+  }
+);
 
 export default FormRow;
--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FormRow } from "..";
-import { CustomButton } from "..";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { logoImage } from "../../constants/ImagesConstant";
+import { CustomButton } from "..";
+import { FormRow } from "..";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formValid, setFormValid] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") setEmail(value);
+    if (name === "password") setPassword(value);
+    setFormValid(email.trim() !== "" && password.trim() !== "");
+  };
+
+  const handleSubmit = () => {
+    if (formValid) {
+      navigate("/dashboard");
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center bg-gray-200 min-h-screen">
       <div className="flex items-center justify-center md:justify-start cursor-default mx-auto pb-4 pt-0">
@@ -17,19 +35,31 @@ const LoginPage = () => {
         <h1 className="text-2xl font-bold text-bluePurple uppercase mb-4 text-center">
           Login
         </h1>
-        <form className="flex flex-col items-center">
-          <FormRow type="Email" name="Email" />
-          <FormRow type="Password" name="Password" />
-          <Link href="/dashboard">
-            <CustomButton
-              className="relative bg-blue-500 text-white font-medium py-[1rem] px-[3.5rem] md:px-[4rem] lg:px-[5rem] mr-0 mb-[20px] md:mb-0 rounded-[3rem] group overflow-hidden z-[1]"
-              title="Login"
-              titleClassName="group-hover:text-white font-subtitle"
-              href="/dashboard"
-            >
-              <div className="absolute inset-0 bg-black w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
-            </CustomButton>
-          </Link>
+        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+          <FormRow
+            type="email"
+            name="email"
+            labelText="Email"
+            onChange={handleInputChange}
+          />
+          <FormRow
+            type="password"
+            name="password"
+            labelText="Password"
+            onChange={handleInputChange}
+          />
+          <CustomButton
+            className={`relative bg-blue-500 text-white font-medium py-[1rem] px-[3.5rem] md:px-[4rem] lg:px-[5rem] mr-0 mb-[20px] md:mb-0 rounded-[3rem] group overflow-hidden z-[1] ${
+              !formValid && "opacity-50 cursor-not-allowed"
+            }`}
+            title="Login"
+            titleClassName="group-hover:text-white font-subtitle"
+            type="submit"
+            disabled={!formValid}
+            onClick={handleSubmit}
+          >
+            <div className="absolute inset-0 bg-black w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
+          </CustomButton>
         </form>
         <div className="pt-4 font-montserrat">
           Not a member yet?
