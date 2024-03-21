765329d51e948da7235fd6b7d69fdb1763de0918
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-21 16:49:45 +0100

    Fixing Sidebar functionality for large and small screens

--- client/src/components/BigSidebar/BigSidebar.jsx
+++ client/src/components/BigSidebar/BigSidebar.jsx
@@ -2,35 +2,40 @@ import React from "react";
 import { useDashboardContext } from "../DashboardPage/DashboardPage";
 import LinksConstant from "../../constants/LinksConstant";
 import { NavLink } from "react-router-dom";
+import { MdClose } from "react-icons/md";
 
 const BigSidebar = () => {
-  const { showBigSidebar, toggleBigSidebar } = useDashboardContext();
+  const { showSmallSidebar, toggleSmallSidebar } = useDashboardContext();
 
   return (
     <div
       className={`${
-        showBigSidebar ? "block" : "hidden"
-      } lg:block fixed inset-y-0 left-0 w-64 bg-white z-50 overflow-y-auto`}
+        showSmallSidebar ? "block" : "hidden"
+      } fixed inset-0 bg-black bg-opacity-20 z-50`}
     >
-      <div className="flex justify-between items-center px-4 py-2 border-b border-gray-300">
-        <h2 className="text-lg font-medium font-montserrat">Hirefy</h2>
-        <button className="lg:hidden" onClick={toggleBigSidebar}></button>
-      </div>
-      <div className="mt-6">
-        <div className="space-y-2">
-          {LinksConstant.map((link) => {
-            const { text, path, icon: Icon } = link;
-            return (
-              <NavLink
-                to={path}
-                key={text}
-                className="flex justify-center items-center text-[1.2rem]"
-              >
-                <Icon />
-                {text}
-              </NavLink>
-            );
-          })}
+      <div className="fixed inset-y-0 left-0 w-64 bg-white z-50 overflow-y-auto">
+        <div className="flex justify-between items-center px-4 py-2 border-b border-gray-300 h-[4rem]">
+          <h2 className="text-lg font-medium font-montserrat">Hirefy</h2>
+          <button className="" onClick={toggleSmallSidebar}>
+            <MdClose />
+          </button>
+        </div>
+        <div className="mt-6">
+          <div className="space-y-2">
+            {LinksConstant.map((link) => {
+              const { text, path, icon: Icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={text}
+                  className="flex justify-start items-center text-[1.2rem] ml-[1rem]"
+                >
+                  <Icon className="mr-[1rem]" />
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/SmallSidebar/SmallSidebar.jsx
+++ client/src/components/SmallSidebar/SmallSidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDashboardContext } from "../DashboardPage/DashboardPage";
 import LinksConstant from "../../constants/LinksConstant";
 import { NavLink } from "react-router-dom";
+import { MdClose } from "react-icons/md";
 
 const SmallSidebar = () => {
   const { showSmallSidebar, toggleSmallSidebar } = useDashboardContext();
@@ -13,9 +14,11 @@ const SmallSidebar = () => {
       } fixed inset-0 bg-black bg-opacity-80 z-50`}
     >
       <div className="fixed inset-y-0 left-0 w-64 bg-white z-50 overflow-y-auto">
-        <div className="flex justify-between items-center px-4 py-2 border-b border-gray-300">
+        <div className="flex justify-between items-center px-4 py-2 border-b border-gray-300 h-[4rem]">
           <h2 className="text-lg font-medium font-montserrat">Hirefy</h2>
-          <button className="lg:hidden" onClick={toggleSmallSidebar}></button>
+          <button className="lg:hidden" onClick={toggleSmallSidebar}>
+            <MdClose />
+          </button>
         </div>
         <div className="mt-6">
           <div className="space-y-2">
@@ -25,9 +28,9 @@ const SmallSidebar = () => {
                 <NavLink
                   to={path}
                   key={text}
-                  className="flex justify-center items-center text-[1.2rem]"
+                  className="flex justify-start items-center text-[1.2rem] ml-[1rem]"
                 >
-                  <Icon />
+                  <Icon className="mr-[1rem]" />
                   {text}
                 </NavLink>
               );
