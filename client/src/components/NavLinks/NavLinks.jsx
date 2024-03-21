b00e39f81ab53a722ba5b2c64fb31bf9c85ed68a
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-21 17:06:28 +0100

    Creating NavLinks component

---
+++ client/src/components/NavLinks/NavLinks.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useDashboardContext } from "../DashboardPage/DashboardPage";
+import LinksConstant from "../../constants/LinksConstant";
+
+const NavLinks = () => {
+  const { toggleSmallSidebar, user } = useDashboardContext();
+  return (
+    <div className="mt-6">
+      <div className="space-y-2">
+        {LinksConstant.map((link) => {
+          const { text, path, icon: Icon } = link;
+          return (
+            <NavLink
+              to={path}
+              key={text}
+              onClick={toggleSmallSidebar}
+              className="flex justify-start items-center text-[1.2rem] ml-[1rem]"
+            >
+              <Icon className="mr-[1rem]" />
+              {text}
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NavLinks;
