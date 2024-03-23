cd2f7d34b3eac53e0f05c47682cf09357d235a82
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-23 16:11:33 +0100

    Installing packages, setting up server and host

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Server is working!");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello server");
+});
+
+app.listen(5100, () => {
+  console.log("Server is running");
+});
