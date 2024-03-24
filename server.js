325891ecb09257801f1ec529daf8613674acbaf2
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-24 03:26:40 +0100

    Installing morgan, setting up node env

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello server");
 });
 
-app.listen(5100, () => {
-  console.log("Server is running");
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "Data recieved", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
 });
