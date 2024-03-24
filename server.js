e55659d6f28eb184e27e9c249a8e0ff1bb409a9f
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-24 17:20:26 +0100

    Setting some local data and allowing access to read all jobs for the user

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 const app = express();
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "Apple", position: "Front-End" },
+  { id: nanoid(), company: "Google", position: "Back-End" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -17,6 +23,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "Data recieved", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
