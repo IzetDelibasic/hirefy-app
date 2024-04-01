9ba83e7da5e610f4139b9f7f574e2e20708f0f75
Izet Delibasic <izetdelibasic55@gmail.com>
2024-04-01 04:37:14 +0200

    Updating and fixing login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/UserModel.js";
 import { StatusCodes } from "http-status-codes";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -14,5 +15,13 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  const user = await User.findOne({ email: req.body.email });
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+
+  if (!isValidUser) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  res.send("Login successful");
 };
