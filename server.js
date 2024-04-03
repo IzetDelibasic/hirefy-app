54de96d078f02ebffcf1a5df4e5ede225331dd7a
Izet Delibasic <izetdelibasic55@gmail.com>
2024-04-03 03:09:51 +0200

    Creating controller and routes for users

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import mongoose from "mongoose";
 // -Routers-
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 // -Middleware-
 import errorHandleMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
