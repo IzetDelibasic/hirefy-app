79ab5f708c77d282042ca0f6b471794f86de61a1
Izet Delibasic <izetdelibasic55@gmail.com>
2024-04-03 04:24:02 +0200

    Creating method for admin app stats

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,10 +5,14 @@ import {
   updateUser,
 } from "../controllers/userController.js";
 import { validateUserUpdateInput } from "../middleware/validationMiddleware.js";
+import { authorizePermissions } from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.get("/current-user", getCurrentUser);
-router.get("/admin/app-stats", getApplicationStats);
+router.get("/admin/app-stats", [
+  authorizePermissions("admin"),
+  getApplicationStats,
+]);
 router.patch("/update-user", validateUserUpdateInput, updateUser);
 
 export default router;
