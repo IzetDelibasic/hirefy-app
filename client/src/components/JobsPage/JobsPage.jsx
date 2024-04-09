87a3cb4d72620fd5afc732287af6a25e8d496a1d
Izet Delibasic <izetdelibasic55@gmail.com>
2024-04-09 03:02:22 +0200

    Fetching all jobs to page

--- client/src/components/JobsPage/JobsPage.jsx
+++ client/src/components/JobsPage/JobsPage.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import customFetch from "../../utils/customFetch";
+import { toast } from "react-toastify";
 
 const JobsPage = () => {
+  useEffect(() => {
+    const getAllJobs = async () => {
+      try {
+        const { data } = await customFetch.get("/jobs");
+        console.log(data);
+        return data;
+      } catch (error) {
+        toast.error(error?.response?.data?.msg);
+        return error;
+      }
+    };
+
+    getAllJobs();
+  }, []);
+
   return <div>JobsPage</div>;
 };
 
