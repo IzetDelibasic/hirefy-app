e0bc4a73140afe6a24241bd5dab97b0cfb1a97d3
Izet Delibasic <izetdelibasic55@gmail.com>
2024-04-18 03:54:36 +0200

    Adding first style for profile page

--- client/src/components/ProfilePage/ProfilePage.jsx
+++ client/src/components/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,7 @@
 // -React-
 import React from "react";
 import { toast } from "react-toastify";
-import { useNavigate, useOutletContext, Form } from "react-router-dom";
+import { useOutletContext, Form } from "react-router-dom";
 // -Components-
 import FormRow from "../FomRow/FormRow";
 // -Utils-
@@ -29,37 +29,73 @@ const ProfilePage = () => {
   if (!user) {
     return <div>Loading user</div>;
   }
-  const { name, email, lastName, location } = user;
+  const { name, email, lastName, location, avatar } = user;
 
   return (
     <div>
-      <Form method="post" encType="multipart/form-data" onSubmit={action}>
-        <div>Profile</div>
-        <div>
-          <label htmlFor="avatar">Select an image file (max 0.5MB)</label>
-          <input type="file" name="avatar" accept="image/*" id="avatar" />
-        </div>
-        <div>
-          <FormRow label="Name" defaultValue={name} name="name" type="text" />
-          <FormRow
-            label="Last Name"
-            defaultValue={lastName}
-            name="lastName"
-            type="text"
-          />
-          <FormRow
-            label="Email"
-            defaultValue={email}
-            name="email"
-            type="text"
-          />
-          <FormRow
-            label="Location"
-            defaultValue={location}
-            name="location"
-            type="text"
-          />
-          <button type="submit">Save changes</button>
+      <Form
+        method="post"
+        encType="multipart/form-data"
+        onSubmit={action}
+        className="max-w-md lg:max-w-xl mx-auto mt-4 font-montserrat"
+      >
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-6">
+            <div className="text-xl font-medium mb-2 text-center">
+              {name} - Profile
+            </div>
+            <label
+              className="block text-black text-sm font-medium mb-2 text-center"
+              htmlFor="avatar"
+            >
+              {user && avatar ? (
+                <img
+                  src={avatar}
+                  alt="avatar"
+                  className="w-[10rem] h-[10rem] rounded-full mx-auto my-2"
+                />
+              ) : (
+                <FaUserCircle className="" />
+              )}
+              Select an image file (max 0.5MB)
+            </label>
+            <input
+              type="file"
+              name="avatar"
+              accept="image/*"
+              id="avatar"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4 flex flex-col justify-center items-center">
+            <FormRow label="Name" defaultValue={name} name="name" type="text" />
+            <FormRow
+              label="Last Name"
+              defaultValue={lastName}
+              name="lastName"
+              type="text"
+            />
+            <FormRow
+              label="Email"
+              defaultValue={email}
+              name="email"
+              type="text"
+            />
+            <FormRow
+              label="Location"
+              defaultValue={location}
+              name="location"
+              type="text"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-800 ease-in-out duration-300 text-white py-2 px-4 focus:outline-none rounded-xl focus:shadow-outline w-[30%]"
+            >
+              Save changes
+            </button>
+          </div>
         </div>
       </Form>
     </div>
